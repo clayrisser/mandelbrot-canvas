123cc78c77b1a852adda1b5579e57ab39e5e9c37
Jam Risser <jam@codejam.ninja>
2019-01-03 14:34:04 -0600

    Added hue and detail sliders

--- example/App.js
+++ example/App.js
@@ -1,17 +1,82 @@
 import React, { Component } from 'react';
+import Slider from 'carbon-components-react/es/components/Slider';
 import Mandelbrot from '../src';
 
 export default class App extends Component {
+  static styles = {
+    parent: {
+      paddingTop: '20px',
+      paddingLeft: '20px'
+    },
+    child: {
+      marginBottom: '20px',
+      marginRight: '20px'
+    }
+  };
+
+  state = {
+    hue: 0,
+    detail: 100,
+    magnification: 10
+  };
+
   componentDidMount() {
-    this.mandelbrot = new Mandelbrot(this.refs.mandelbrot);
+    this.mandelbrot = new Mandelbrot(this.refs.mandelbrot, {
+      width: 1000
+    });
     this.mandelbrot.render();
   }
 
   render() {
+    const { detail, hue, magnification } = this.state;
     return (
-      <div>
+      <div style={App.styles.parent}>
         <h1>Mandelbrot Canvas</h1>
-        <div ref="mandelbrot" />
+        <div
+          style={{
+            display: 'flex'
+          }}
+        >
+          <div style={App.styles.child} ref="mandelbrot" />
+          <div>
+            <div style={App.styles.child}>
+              <Slider
+                labelText="Hue"
+                min={0}
+                max={366}
+                value={hue}
+                onMouseUp={() => this.mandelbrot.setState({ hue })}
+                onChange={({ value }) => this.setState({ hue: value })}
+              />
+            </div>
+            <div style={App.styles.child}>
+              <Slider
+                labelText="Detail"
+                min={0}
+                max={500}
+                value={detail}
+                onMouseUp={() => this.mandelbrot.setState({ detail })}
+                onChange={({ value }) => this.setState({ detail: value })}
+              />
+            </div>
+            <div style={App.styles.child}>
+              <Slider
+                labelText="Magnification"
+                min={0}
+                max={1000}
+                value={magnification}
+                onMouseUp={() =>
+                  this.mandelbrot.setState({
+                    magnification: magnification * magnification + 50
+                  })
+                }
+                onChange={({ value }) =>
+                  this.setState({ magnification: value })
+                }
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
+import 'carbon-components/css/carbon-components.css';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
